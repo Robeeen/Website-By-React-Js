ae1cd27682300287feb0865c38143b335120360b
Shams Arefin Khan <robeeen@gmail.com>
2021-11-27 08:53:40 +0600

    Menu part

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Header() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <a href="/"><img src="/images/logo.svg" alt="Logo"/></a>
+            <Menu>
+
+            </Menu>
+
+        </Container>
     )
 }
 
 export default Header
+
+const Container = styled.div`
+    min-height: 60px;
+`
+const Menu = styled.div`
+
+
+`
